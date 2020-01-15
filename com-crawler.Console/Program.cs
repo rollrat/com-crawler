957de77c3eb2fadfc82ec20e92c5bf595bb8f54c
rollrat <rollrat@inha.edu>
2020-01-15 13:14:06 +0900

    [project] Add com-crawler.Console project

---
+++ com-crawler.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com_crawler.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
