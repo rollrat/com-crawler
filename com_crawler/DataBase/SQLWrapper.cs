36cd530150c6e3a74603718244832e481a3ce25e
rollrat <rollrat@inha.edu>
2020-01-19 12:15:43 +0900

    [DB] Install package mysql.data

--- com_crawler/DataBase/SQLWrapper.cs
+++ com_crawler/DataBase/SQLWrapper.cs
@@ -3,25 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace com_crawler.DataBase
 {
-    public abstract class SQLWrapper
+    public class SQLWrapper
     {
-        public static SQLWrapper Instance;
-
-        protected SQLWrapper(string connection_string) { }
-        public abstract int EvalCount(string table, string attr);
-        public abstract void EvalNqSql(string sql);
-        public abstract void EvalReadSql<T>(string sql, Action<DbDataReader> data_reader);
-    }
-
-    public abstract class IDataBase<T>
-    {
-        public abstract void Insert(T cxt);
-        public abstract void Update(T cxt);
-        public abstract void Remove(T cxt);
-        public abstract List<T> Query(string where = "");
     }
 }
