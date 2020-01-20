9c80aeebbc38e7472e71939cedbfcbcec1a28322
rollrat <rollrat@inha.edu>
2020-01-20 20:29:17 +0900

    [Component/Community] Implements community model methods.

--- com_crawler/Component/CommunityModel.cs
+++ com_crawler/Component/CommunityModel.cs
@@ -71,6 +71,24 @@ namespace com_crawler.Component
         public List<int> Links;
     }
 
+    public class CommunityComment
+    {
+        public int Index;
+
+        public int Article;
+
+        public string Writer;
+        public string WriteTime;
+        public string WriteId;
+
+        public string UpVote;
+        public string DownVote;
+
+        public string Body;
+
+        public List<int> SubComment;
+    }
+
     public class CommunityArticle
     {
         /// <summary>
@@ -79,6 +97,11 @@ namespace com_crawler.Component
         public int Index;
 
         public int Board;
+
+        /// <summary>
+        /// This instance not contains body.
+        /// </summary>
+        public bool IsHeaderOnly = true;
 
         public string Id;
 
@@ -95,6 +118,8 @@ namespace com_crawler.Component
 
         public string Body;
 
+        public List<int> Comment;
+
         public virtual string MakeURL() { throw new NotImplementedException(); }
     }
 
@@ -103,6 +128,20 @@ namespace com_crawler.Component
         public int Index;
         public int BoardMap;
         public List<int> Articles;
+    }
+
+    public class CommunityLoginInfo
+    {
+        public string Index;
+        public bool IsAnonymous;
+        public string Name;
+
+        /// <summary>
+        /// If `IsAnonymous` flag is not true, then
+        /// </summary>
+        public string Id;
+        public string Password;
+        public string Token;
     }
 
     public class CommunityContext
@@ -130,6 +169,9 @@ namespace com_crawler.Component
 
         public List<CommunityBoard> Board;
         public List<CommunityArticle> Article;
+        public List<CommunityComment> Comment;
+
+        public List<CommunityLoginInfo> LoginInfo;
     }
 
     public abstract class CommunityModel : ComponentModel
@@ -142,5 +184,29 @@ namespace com_crawler.Component
         }
 
         public abstract void BuildSitemap();
+
+        /// <summary>
+        /// API Implementations
+        /// </summary>
+        
+        public abstract void WriteArticle(CommunitySitemapBoard board, string title, string body, CommunityLoginInfo loginfo);
+        public abstract void DeleteArticle(CommunityArticle article, CommunityLoginInfo loginfo);
+
+        public abstract void WriteComment(CommunityArticle article, string body, CommunityLoginInfo loginfo);
+        public abstract void WriteComment(CommunityComment comment, string body, CommunityLoginInfo loginfo);
+        public abstract void DeleteComment(CommunityComment article, CommunityLoginInfo loginfo);
+
+        public abstract void UpVoteArticle(CommunityArticle article, CommunityLoginInfo loginfo);
+        public abstract void DownVoteArticle(CommunityArticle article, CommunityLoginInfo loginfo);
+
+        /// <summary>
+        /// Parser Implementations
+        /// </summary>
+
+        public abstract int GetMaximumBoardPage(CommunityBoard board);
+
+        public abstract List<int> ParseBoard(CommunityBoard board, int page);
+        public abstract void ParseArticle(CommunityArticle article);
+        public abstract List<int> ParseComment(CommunityArticle article);
     }
 }
