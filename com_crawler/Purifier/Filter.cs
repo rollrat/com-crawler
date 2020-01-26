f9e69cdc224e63dac0f9f19c62cb4dce93092911
rollrat <rollrat@inha.edu>
2020-01-26 23:48:29 +0900

    [Purifier/Filter] Add filter prototype, header

---
+++ com_crawler/Purifier/Filter.cs
@@ -0,0 +1,20 @@
+﻿// This source code is a part of Community Crawler Project.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace com_crawler.Purifier
+{
+    public class Filter
+    {
+        static Regex regex_email = new Regex(@"[\w!#$%&'*+/=?`{|}~^.-]+@[A-Z0-9.-]+");
+
+        public static string PersonalInformation(string str)
+        {
+            return regex_email.Replace(str, "*@*.*");
+        }
+    }
+}
