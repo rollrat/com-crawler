fe52f4523c61807768fbec294fb87df3e933707b
rollrat <rollrat@inha.edu>
2020-01-26 23:48:02 +0900

    [Bot] Add bot class header

---
+++ com_crawler/Bot/TelegramBot.cs
@@ -0,0 +1,13 @@
+﻿// This source code is a part of Community Crawler Project.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com_crawler.Bot
+{
+    public class TelegramBot
+    {
+    }
+}
