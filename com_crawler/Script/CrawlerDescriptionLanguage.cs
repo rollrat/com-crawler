952b59406a57665ad9fc6d0475a93d7e320eba34
rollrat <rollrat@inha.edu>
2020-02-27 23:25:04 +0900

    [Script] Add Crawler description language parser prototype

---
+++ com_crawler/Script/CrawlerDescriptionLanguage.cs
@@ -0,0 +1,29 @@
+﻿// This source code is a part of Community Crawler Project.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using com_crawler.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com_crawler.Script
+{
+    public class CrawlerDescriptionLanguage
+    {
+        static ShiftReduceParser parser;
+        public static ShiftReduceParser GetParser()
+        {
+            if (parser != null)
+                return parser;
+            return null;
+        }
+
+        static Scanner scanner;
+        public static Scanner GetScanner()
+        {
+            if (scanner != null)
+                return scanner;
+            return null;
+        }
+    }
+}
