2ed84d8c9673d8877f5ac06b2e5793a93ffbd8fc
rollrat <rollrat@inha.edu>
2020-03-05 01:02:17 +0900

    [Bot] Add crawl bot manager header

---
+++ com_crawler/Bot/BotManager.cs
@@ -0,0 +1,19 @@
+﻿// This source code is a part of Community Crawler Project.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using com_crawler.Component;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com_crawler.Bot
+{
+    /// <summary>
+    /// Crawling bot manager
+    /// </summary>
+    public class BotManager
+    {
+        List<ComponentModel> components;
+
+    }
+}
